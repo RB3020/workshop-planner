4ed982d510a059599cc3a5540eb85477bc5a797d
RB3020 <bain3020@hotmail.com>
2026-01-28 22:36:36 +0000

    Delete src/main.jsx

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WorkshopPlanner from './WorkshopPlanner.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WorkshopPlanner />
-  </React.StrictMode>,
-)
